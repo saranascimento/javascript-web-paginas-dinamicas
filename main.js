150d640692bef79b105a602a871e97f69b497d22
Sara Gomes <sara.gomes@g.globo>
2022-09-18 17:51:18 -0300

    iterating array + fourth class completed

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
-function playPomSound() {
-  document.querySelector("#sound_key_pom").play();
+function playSound(audioElementId) {
+  document.querySelector(audioElementId).play();
 }
 
 const keyList = document.querySelectorAll(".key");
 
-keyList[0].onclick = playPomSound;
+/* loop while
+let count = 0;
+
+while (count < keyList.length) {
+  const key = keyList[count];
+
+  const instrument = key.classList[1];
+
+  const audioId = `#sound_${instrument}`;
+
+  key.onclick = function () {
+    playSound(audioId);
+  };
+
+  count = count + 1;
+}
+*/
+
+// loop for
+for (let count = 0; count < keyList.length; count++) {
+  const key = keyList[count];
+  const instrument = key.classList[1];
+  const audioId = `#sound_${instrument}`;
+
+  key.onclick = function () {
+    playSound(audioId);
+  };
+}
