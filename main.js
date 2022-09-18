bb799b0cbba9c806c115d6a79cd33b6ca39f35c4
Sara Gomes <sara.gomes@g.globo>
2022-09-18 20:00:44 -0300

    events and logic + course completed

--- main.js
+++ main.js
@@ -1,28 +1,15 @@
-function playSound(audioElementId) {
-  document.querySelector(audioElementId).play();
+function playSound(audioSelector) {
+  const element = document.querySelector(audioSelector);
+
+  if (element && element.localName === "audio") {
+    element.play();
+  } else {
+    console.log("Elemento não encontrado ou seletor inválido");
+  }
 }
 
 const keyList = document.querySelectorAll(".key");
 
-/* loop while
-let count = 0;
-
-while (count < keyList.length) {
-  const key = keyList[count];
-
-  const instrument = key.classList[1];
-
-  const audioId = `#sound_${instrument}`;
-
-  key.onclick = function () {
-    playSound(audioId);
-  };
-
-  count = count + 1;
-}
-*/
-
-// loop for
 for (let count = 0; count < keyList.length; count++) {
   const key = keyList[count];
   const instrument = key.classList[1];
@@ -30,5 +17,17 @@ for (let count = 0; count < keyList.length; count++) {
 
   key.onclick = function () {
     playSound(audioId);
+  };
+
+  key.onkeydown = function (event) {
+    if (event.code === "Space" || event.code === "Enter") {
+      key.classList.add("active");
+    }
+  };
+
+  key.onkeyup = function (event) {
+    if (event.code === "Space" || event.code === "Enter") {
+      key.classList.remove("active");
+    }
   };
 }
